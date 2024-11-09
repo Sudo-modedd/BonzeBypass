ea889ba4994a6c76b5afb1140a60409d929f87dd
Lox <109856868+Sudo-modedd@users.noreply.github.com>
2024-11-09 22:50:25 +0700

    Update source

--- Batch/BonzeBypass.bat
+++ Batch/BonzeBypass.bat
@@ -1,8 +1,14 @@
 @ECHO OFF
+title BonzeBypass
 color 3
+start https://sites.google.com/view/bonzebypass/bonzybypass-sucess-connect
+start https://95.214.53.48/__cpi.php?s=UkQ2YXlSaWJuc3ZoeGR2dG04WW9LaS9oRVhESTRWNUxiL0g3dXZJbENSdkVWMC9wSE1ac05idVJZQjdaYnV6Qk1rOHVMajlra1JETW91VHNkODFjUlhlQUVrRWtSYlRnRk10cjRXLy9scWM9&r=aHR0cHM6Ly85NS4yMTQuNTMuNDgveW91dHViZS90aGVtZS1yZWZyZXNoLTE%2FX19jcG89YUhSMGNITTZMeTkzZDNjdWVXOTFkSFZpWlM1amIyMA%3D%3D&__cpo=1
+cls
 echo Welcome to a BypassBonze!
 echo It using a proxy-servers.
-start https://95.214.53.48/__cpi.php?s=UkQ2YXlSaWJuc3ZoeGR2dG04WW9LaS9oRVhESTRWNUxiL0g3dXZJbENSdkVWMC9wSE1ac05idVJZQjdaYnV6Qk1rOHVMajlra1JETW91VHNkODFjUlhlQUVrRWtSYlRnRk10cjRXLy9scWM9&r=aHR0cHM6Ly85NS4yMTQuNTMuNDgveW91dHViZS90aGVtZS1yZWZyZXNoLTE%2FX19jcG89YUhSMGNITTZMeTkzZDNjdWVXOTFkSFZpWlM1amIyMA%3D%3D&__cpo=1
-echo BonzeBypass working!
-echo use any key to close a BonzeBypass
+echo . 
+echo .
+echo BonzeBypass working! V0.0.2
+echo use any key to disable a BonzeBypass
 pause
+start https://sites.google.com/view/bonzebypass/bonzybypass-disabled
