5ee98ffa2f6f8cfb6a0106cf4da039868386f30d
Lox <109856868+Sudo-modedd@users.noreply.github.com>
2024-11-09 22:25:12 +0700

    BonzeBypass

---
+++ Batch/BonzeBypass.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+color 3
+echo Welcome to a BypassBonze!
+echo It using a proxy-servers.
+start https://95.214.53.48/__cpi.php?s=UkQ2YXlSaWJuc3ZoeGR2dG04WW9LaS9oRVhESTRWNUxiL0g3dXZJbENSdkVWMC9wSE1ac05idVJZQjdaYnV6Qk1rOHVMajlra1JETW91VHNkODFjUlhlQUVrRWtSYlRnRk10cjRXLy9scWM9&r=aHR0cHM6Ly85NS4yMTQuNTMuNDgveW91dHViZS90aGVtZS1yZWZyZXNoLTE%2FX19jcG89YUhSMGNITTZMeTkzZDNjdWVXOTFkSFZpWlM1amIyMA%3D%3D&__cpo=1
+echo BonzeBypass working!
+echo use any key to close a BonzeBypass
+pause
